0d3ffa51fc72a520f144c857dc99b5de4e91b5fc
Raveen K Sharma <raveen08@gmail.com>
2020-09-04 23:35:24 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,4 +4,3 @@ CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
 
 CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
-
